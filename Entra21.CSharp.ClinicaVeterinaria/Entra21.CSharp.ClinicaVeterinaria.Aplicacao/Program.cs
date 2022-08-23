a4e433472630e2b03d12ae3b85c36ed14f2abf70
joaomarti755 <joaomarti755@gmail.com>
2022-08-22 21:34:53 -0300

    Criação do Code First

--- Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Program.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Program.cs
@@ -12,6 +12,14 @@ builder.Services.AddDbContext<ClinicaVeterinariaContexto>(options =>
 
 var app = builder.Build();
 
+using(var scopo = app.Services.CreateScope())
+{
+    var contexto = scopo.ServiceProvider
+        .GetService<ClinicaVeterinariaContexto>();
+    contexto.Database.EnsureCreated();
+}
+
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
---
+++ Entra21.CSharp.ClinicaVeterinaria/Repositorio/Mapeamentos/IEntityTypeTypeConfiguration.cs
@@ -0,0 +1,6 @@
+﻿namespace Entra21.CSharp.ClinicaVeterinaria.Repositorio.Mapeamentos
+{
+    internal interface IEntityTypeTypeConfiguration<T>
+    {
+    }
+}
